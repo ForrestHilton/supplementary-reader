674d58e6d9ff8568411a8bd7ca6096125a20a492
Forrest  Hilton <forrestmhilton@gmail.com>
2021-10-04 15:59:31 -0500

    spd-say version

--- do.bash
+++ do.bash
@@ -4,6 +4,5 @@ image_name="$(ls -u ~/Pictures/ | head -n 1)"
 
 tesseract ~/Pictures/"$image_name" /tmp/image-to-voice --oem 0
 
-cat /tmp/image-to-voice | tr '\\n' " " > /tmp/image-to-voice
-espeak-ng "$(cat /tmp/image-to-voice.txt)" && trash ~/Pictures/"$image_name"
+cat /tmp/image-to-voice.txt | spd-say -e -w && trash ~/Pictures/"$image_name"
 
