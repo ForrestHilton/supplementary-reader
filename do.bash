be286cbcf43ee35c891323f09309fee430608b1a
forrest <forrestmhilton@gmail.com>
2021-12-21 18:39:13 -0600

    added selection capture and some documentation and changed the name

--- do.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm /tmp/image-to-text-pause
-
-gnome-screenshot -a -f /tmp/image-to-voice.png
-
-tesseract /tmp/image-to-voice.png /tmp/image-to-voice --oem 0
-
-python3 ~/Projects/image-to-voice/do.py
